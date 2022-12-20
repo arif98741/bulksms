1a56ceff74801eef809c51f6c9694f98c8456965
Ariful Islam <ariful@noobyia.email>
2022-12-20 12:56:15 +0600

    contact list updated

--- app/AppTrait/QueryTrait.php
+++ app/AppTrait/QueryTrait.php
@@ -3,9 +3,25 @@
 namespace App\AppTrait;
 
 use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 trait QueryTrait
 {
+    use AuthTrait;
+
+    /**
+     * Data Existance Check for Individual User
+     * @param string $table
+     * @param array $where
+     * @return bool
+     */
+    public function isDataExist(string $table, array $where)
+    {
+        $where['created_by'] = self::getUserId();
+        $count = DB::table($table)->where($where)->count();
+        return $count !== 0;
+    }
+
     /**
      * @param object $q
      * @param string $queryColumn
@@ -15,40 +31,29 @@ trait QueryTrait
      */
     public function dateRangeFilter(object $q, string $queryColumn = 'created_at', string $startDate = null, string $endDate = null): object
     {
-        $carbon = Carbon::now();
-        switch (\request('date_filter')) {
-            case 'today':
-                return $q->whereRaw("Date($queryColumn) = CURDATE()"); //today
-            case 'yesterday':
-                return $q->whereRaw("Date($queryColumn) = CURDATE() - INTERVAL 1 DAY"); //yesterday
-            case 'last_3':
-                return $q->whereBetween($queryColumn, [Carbon::now()->subDays(2), Carbon::now()]);
-            case 'last_7':
-                return $q->whereBetween($queryColumn, [Carbon::now()->subDays(6), Carbon::now()]);
-            case 'last_15':
-                return $q->whereBetween($queryColumn, [Carbon::now()->subDays(14), Carbon::now()]);
-            case 'this_m':
-                return $q->whereBetween($queryColumn, [
-                    Carbon::now()->startOfMonth(),
-                    Carbon::now()->endOfMonth()
-                ]);
-            case 'pre_m':
-                return $q->whereBetween($queryColumn, [
-                    Carbon::now()->subMonths(1)->startOfMonth(),
-                    Carbon::now()->subMonths(1)->endOfMonth()
-                ]);
-            case 'last_3m':
-
-                return $q->whereBetween($queryColumn, [
-                    Carbon::now()->subMonths(2)->startOfMonth(),
-                    Carbon::now()->endOfMonth()
-                ]);
-            case 'custom':
-                return $q->whereBetween($queryColumn, [
-                    $startDate,
-                    $endDate
-                ]);
-        }
-        return $q;
+        return match (\request('date_filter')) {
+            'today' => $q->whereRaw("Date($queryColumn) = CURDATE()"),
+            'yesterday' => $q->whereRaw("Date($queryColumn) = CURDATE() - INTERVAL 1 DAY"),
+            'last_3' => $q->whereBetween($queryColumn, [Carbon::now()->subDays(2), Carbon::now()]),
+            'last_7' => $q->whereBetween($queryColumn, [Carbon::now()->subDays(6), Carbon::now()]),
+            'last_15' => $q->whereBetween($queryColumn, [Carbon::now()->subDays(14), Carbon::now()]),
+            'this_m' => $q->whereBetween($queryColumn, [
+                Carbon::now()->startOfMonth(),
+                Carbon::now()->endOfMonth()
+            ]),
+            'pre_m' => $q->whereBetween($queryColumn, [
+                Carbon::now()->subMonths(1)->startOfMonth(),
+                Carbon::now()->subMonths(1)->endOfMonth()
+            ]),
+            'last_3m' => $q->whereBetween($queryColumn, [
+                Carbon::now()->subMonths(2)->startOfMonth(),
+                Carbon::now()->endOfMonth()
+            ]),
+            'custom' => $q->whereBetween($queryColumn, [
+                $startDate,
+                $endDate
+            ]),
+            default => $q,
+        };
     }
 }
